250a943fb12ed8b2317722becf80cfe6d4d54b1a
Domnic Omondi <domooh9@gmail.com>
2022-06-04 22:34:13 +0300

    commit tree a3c2f90c198759237a8f9dcb442b2eddd5861cdb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/* const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts) */
+const cards = ["Lisa", "Kaitlyn", "Jan", "surprise"];
+
+const newArray = [];
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+return newArray
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(4);
